23387e21f766d473684de6bacf189f96cad238ab
Burak123T <burak-9@live.dk>
2025-11-19 13:31:15 +0100

    minor changes

--- src/client_logs.c
+++ src/client_logs.c
@@ -8,7 +8,7 @@
 void set_log_param(LOG_CODE log_code)
 {
     uint32_t get_code_val = (uint32_t)log_code;
-    param_set_uint32(&upload_log_status, get_code_val);
+    param_set_uint32(&get_upload_log_status, get_code_val);
     if (get_code_val != UPLOAD_SUCCESS) {
         printf("\t%s - [ERROR] Logged error code: %d %s\n", "\x1B[31m", get_code_val, "\x1B[0m");
     }
