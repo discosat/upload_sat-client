a7f021c13718212689abdaab1375928ec0cff30e
Burak123T <burak-9@live.dk>
2025-11-25 13:03:20 +0100

    changed vmem storage name

--- src/vmem/vmem_storage.c
+++ src/vmem/vmem_storage.c
@@ -1,4 +1,4 @@
 #include <vmem/vmem_file.h>
 
 /* Define file to store persistent params */
-VMEM_DEFINE_FILE(storage, "storage", "client_storage.bin.vmem", 10000);
+VMEM_DEFINE_FILE(storage, "storage", "storage.vmem", 10000);
