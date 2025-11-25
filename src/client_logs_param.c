f9f5d81957464833e103482f0ddb11ec5ee1b070
Burak123T <burak-9@live.dk>
2025-11-25 13:32:44 +0100

    name change

--- src/client_logs_param.c
+++ src/client_logs_param.c
@@ -5,4 +5,4 @@
 
 // Define params here...
 
-PARAM_DEFINE_STATIC_VMEM(CLIENT_STATUS_LOG, get_upload_log_status, PARAM_TYPE_UINT32, -1, 0, PM_READONLY, NULL, NULL, storage, VMEM_DOWNLOAD_LOG_ADDR, "Latest upload log code");
+PARAM_DEFINE_STATIC_VMEM(CLIENT_STATUS_LOG, get_upload_log_status, PARAM_TYPE_UINT32, -1, 0, PM_READONLY, NULL, NULL, storage, VMEM_UPLOAD_LOG_ADDR, "Latest upload log code");
--- src/include/vmem/vmem_storage.h
+++ src/include/vmem/vmem_storage.h
@@ -4,6 +4,6 @@
 #include <vmem/vmem.h>
 extern vmem_t vmem_storage;
 
-#define VMEM_DOWNLOAD_LOG_ADDR 0x00
+#define VMEM_UPLOAD_LOG_ADDR 0x00
 
 #endif
