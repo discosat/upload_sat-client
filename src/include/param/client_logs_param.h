3392d2c3538ab8b2f1e8111ef4b8e1bbfa7b1587
Burak123T <burak-9@live.dk>
2025-11-27 13:13:04 +0100

    minor change

--- src/include/param/client_logs_param.h
+++ src/include/param/client_logs_param.h
@@ -11,7 +11,5 @@ extern param_t get_upload_log_status;
 
 #define PARAM_MAX_SIZE 512
 
-PARAM_DEFINE_STATIC_VMEM(CLIENT_STATUS_LOG, get_upload_log_status, PARAM_TYPE_UINT32, PARAM_MAX_SIZE, 0, PM_READONLY, NULL, NULL, storage, VMEM_UPLOAD_LOG_ADDR, "Latest upload log code");
-
 
 #endif
