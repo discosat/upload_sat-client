7f50e4a54b1cfe058ffa09a875a98ebab5c39eb6
Burak123T <burak-9@live.dk>
2025-12-01 11:07:48 +0100

    lets see if this helps...

--- src/include/param/client_logs_param.h
+++ src/include/param/client_logs_param.h
@@ -10,6 +10,6 @@ extern param_t get_upload_log_status;
 #define CLIENT_STATUS_LOG 1
 
 void client_logs_param_init(void);
-
+void set_client_log_status(uint32_t status);
 
 #endif
--- src/param/client_logs_param.c
+++ src/param/client_logs_param.c
@@ -8,6 +8,12 @@ static uint32_t _get_upload_log_status = 0;
 PARAM_DEFINE_STATIC_RAM(CLIENT_STATUS_LOG, get_upload_log_status, PARAM_TYPE_UINT32, -1, 0, PM_READONLY, NULL, NULL, &_get_upload_log_status, "Latest upload log code");
 
 // Registration function
-void client_logs_param_init(void) {
+void client_logs_param_init(void)
+{
     param_list_add(&get_upload_log_status);
+}
+
+// Helper to set value
+void set_client_log_status(uint32_t status) {
+    _get_upload_log_status = status;
 }
