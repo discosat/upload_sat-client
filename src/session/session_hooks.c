4ef612e4e1b443e0ff4cb74729bf692e29db0b80
Burak123T <burak-9@live.dk>
2025-12-04 13:18:03 +0100

    chagnes

--- src/session/session_hooks.c
+++ src/session/session_hooks.c
@@ -12,6 +12,7 @@
 #include "vmem/vmem_mmap.h"
 #include "vmem/vmem_file.h"
 #include "client_logs.h"
+#include "client_logs_param.h"
 #include "client_logs_paramids.h"
 
 #include "vmem/vmem_storage.h"
@@ -22,12 +23,7 @@
 VMEM_DEFINE_MMAP(dtp_upload_session_meta, "dtp_upload_session_meta.bin", "dtp_upload_session_meta.bin", 1024);
 VMEM_DEFINE_MMAP(dtp_upload_data, "dtp_upload_data.bin", "upload_data.bin", 1024);
 
-// PARAM declaration: upload (error log) state
-param_t upload_state;
-
-uint8_t _upload_log;
-
-PARAM_DEFINE_STATIC_VMEM(CLIENT_STATUS_LOG, upload_state, PARAM_TYPE_DATA, 188, 0, PM_CONF, upload_logs_init, NULL, client_storage, VMEM_UPLOAD_LOG_ADDR, "Upload Client status error log");
+PARAM_DEFINE_STATIC_VMEM(CLIENT_STATUS_LOG, remote_upload_log_status, PARAM_TYPE_DATA, 188, 0, PM_CONF, upload_logs_init, NULL, client_storage, VMEM_UPLOAD_LOG_ADDR, "Upload Client status error log");
 
 static void apm_on_start(dtp_t *session);
 static bool apm_on_data_packet(dtp_t *session, csp_packet_t *p);
