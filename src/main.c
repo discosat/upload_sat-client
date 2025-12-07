15c42727b848897ca3d8f450bfa4cba03a878c53
Burak123T <burak-9@live.dk>
2025-12-07 17:33:13 +0100

    changing ports again

--- src/main.c
+++ src/main.c
@@ -42,7 +42,7 @@
 PARAM_DEFINE_STATIC_VMEM(CLIENT_STATUS_LOG, remote_upload_log_status, PARAM_TYPE_DATA, 188, 0, PM_CONF, NULL, NULL, client_storage, VMEM_UPLOAD_LOG_ADDR, "Upload Client status error log");
 
 /* Server port, the port the server listens on for incoming connections from the client. */
-#define CLIENTPORT 20
+#define CLIENTPORT 10
 
 dtp_opt_session_hooks_cfg default_session_hooks;
 extern dtp_opt_session_hooks_cfg apm_session_hooks;
@@ -347,7 +347,7 @@ int main(int argc, char *argv[])
 	csp_conf.hostname = HOSTNAME;
 	csp_init();
 
-	csp_bind_callback(param_serve, PARAM_PORT_SERVER);
+	csp_bind_callback(param_serve, 12);
 	//csp_bind_callback(csp_service_handler, CSP_ANY);
 
 	/* Start router */
@@ -364,7 +364,7 @@ int main(int argc, char *argv[])
 	vmem_file_init(&vmem_client_storage);
 
 	static pthread_t vmem_server_handle;
-    //pthread_create(&vmem_server_handle, NULL, &vmem_server_task, NULL);
+    pthread_create(&vmem_server_handle, NULL, &vmem_server_task, NULL);
 
 	/* Add interface(s) */
 	default_iface = add_interface(device_type, device_name);
