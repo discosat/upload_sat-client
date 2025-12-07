ee79d8dcc6f7ce64d9f41fa344168537516bbe79
Burak123T <burak-9@live.dk>
2025-12-07 17:17:10 +0100

    remove 'csp_bind_callback'

--- src/main.c
+++ src/main.c
@@ -348,7 +348,7 @@ int main(int argc, char *argv[])
 	csp_init();
 
 	csp_bind_callback(param_serve, PARAM_PORT_SERVER);
-	csp_bind_callback(csp_service_handler, CSP_ANY);
+	//csp_bind_callback(csp_service_handler, CSP_ANY);
 
 	/* Start router */
 	router_start();
